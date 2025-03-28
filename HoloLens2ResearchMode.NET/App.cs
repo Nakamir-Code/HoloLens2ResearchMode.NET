d5f500237e831b2c77911041689b9ee88bfc6a06
Austin Hale <haleau@gmail.com>
2025-03-27 22:45:37 -0700

    feat: POC showing how to reference a WinRT component from net9.0 with CsWinRT!

---
+++ HoloLens2ResearchMode.NET/App.cs
@@ -0,0 +1,21 @@
+﻿using HoloLens2ResearchMode;
+using StereoKit;
+using System;
+using System.ComponentModel;
+using Windows.Foundation;
+
+SK.Initialize();
+
+ResearchModeSensorDevice sensorDevice = new();
+ResearchModeSensorConsent requestCameraAccessTask = await sensorDevice.RequestCameraAccessAsync().AsTask();
+
+Model model = Model.FromFile("Watermelon.glb");
+Material floor = new("Floor.hlsl");
+floor.Transparency = Transparency.Blend;
+floor["color"] = new Color(1, 1, 1, 1);
+
+SK.Run(() =>
+{
+	Mesh.Cube.Draw(floor, Matrix.TS(0, -1.5f, 0, V.XYZ(30, 0.01f, 30)));
+	model.Draw(Matrix.TR(V.XYZ(0, 0, -0.5f), Quat.LookDir(0, 0, 1)));
+});
